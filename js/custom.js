f6ecfaf2dec2204a5b87365e3902b150a1341db9
abdelrahman-gad <abdelrahmangad95@gmail.com>
2019-08-18 14:43:11 +0200

    adding paralllax .gallary,counter

--- js/custom.js
+++ js/custom.js
@@ -22,12 +22,59 @@
       });
 // end navigation tggling on resizing the viewport window
 
-// start banner slider
+// gallary
+// MDB Lightbox Init
 
-// end banner slider
+  $(".img-w").each(function() {
+    $(this).wrap("<div class='img-c'></div>")
+    let imgSrc = $(this).find("img").attr("src");
+     $(this).css('background-image', 'url(' + imgSrc + ')');
+  })
+            
+  
+  $(".img-c").click(function() {
+    let w = $(this).outerWidth()
+    let h = $(this).outerHeight()
+    let x = $(this).offset().left
+    let y = $(this).offset().top
+    
+    
+    $(".active").not($(this)).remove()
+    let copy = $(this).clone();
+    copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
+    $(".active").css('top', y - 8);
+    $(".active").css('left', x - 8);
+    
+      setTimeout(function() {
+    copy.addClass("positioned")
+  }, 0)
+    
+  }) 
+  
+  
+
+ 
+
+$(document).on("click", ".img-c.active", function() {
+  let copy = $(this)
+  copy.removeClass("positioned active").addClass("postactive")
+  setTimeout(function() {
+    copy.remove();
+  }, 500)
+})
+
+// end gallary
 
 
 
+ /* ==========================================================================
+      COUNTER UP 
+ ========================================================================== */
+
+ $('.counter').counterUp({
+  delay: 10,
+  time: 1000
+});
 
 
 
