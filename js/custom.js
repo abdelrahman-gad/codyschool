719dcf01a30e63dc4079bb110c55caceeddf349e
abdelrahman-gad <abdelrahmangad95@gmail.com>
2019-08-21 17:11:29 +0200

    fix navbar bug

--- js/custom.js
+++ js/custom.js
@@ -85,5 +85,42 @@ $( '.slideshow' ).cycle();
     }); // end DOM ready
   })(jQuery); // end jQuery
 
+  let mainNav = document.getElementById('js-menu');
+  let navBarToggle = document.getElementById('js-navbar-toggle');
+  
+  navBarToggle.addEventListener('click', function () {
+      
+      mainNav.classList.toggle('active');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
