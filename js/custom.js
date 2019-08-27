a4e0af2f548c63bac3f72a9881079858cbbd2250
abdelrahman-gad <abdelrahmangad95@gmail.com>
2019-08-27 16:05:00 +0200

    fix navbar

--- js/custom.js
+++ js/custom.js
@@ -1,116 +1,12 @@
 // navbar and dropdown
-(function($) { // Begin jQuery
-    $(function() { // DOM ready
-      // If a link has a dropdown, add sub menu toggle.
-      $('nav ul li a:not(:only-child)').click(function(e) {
-        $(this).siblings('.nav-dropdown').toggle();
-        // Close one dropdown when selecting another
-        $('.nav-dropdown').not($(this).siblings()).hide();
-        e.stopPropagation();
-      });
-      // Clicking away from dropdown will remove the dropdown class
-      $('html').click(function() {
-        $('.nav-dropdown').hide();
-      });
-      // Toggle open and close nav styles on click
-      $('#nav-toggle').click(function() {
-        $('nav ul').slideToggle();
-      });
-      // Hamburger to X toggle
-      $('#nav-toggle').on('click', function() {
-        this.classList.toggle('active');
-      });
-// end navigation tggling on resizing the viewport window
-
-// gallary
-// MDB Lightbox Init
-
-  $(".img-w").each(function() {
-    $(this).wrap("<div class='img-c'></div>")
-    let imgSrc = $(this).find("img").attr("src");
-     $(this).css('background-image', 'url(' + imgSrc + ')');
-  })
-            
-  
-  $(".img-c").click(function() {
-    let w = $(this).outerWidth()
-    let h = $(this).outerHeight()
-    let x = $(this).offset().left
-    let y = $(this).offset().top
-    
-    
-    $(".active").not($(this)).remove()
-    let copy = $(this).clone();
-    copy.insertAfter($(this)).height(h).width(w).delay(500).addClass("active")
-    $(".active").css('top', y - 8);
-    $(".active").css('left', x - 8);
-    
-      setTimeout(function() {
-    copy.addClass("positioned")
-  }, 0)
-    
-  }) 
-  
-  
-
- 
-
-$(document).on("click", ".img-c.active", function() {
-  let copy = $(this)
-  copy.removeClass("positioned active").addClass("postactive")
-  setTimeout(function() {
-    copy.remove();
-  }, 500)
-})
-
-// end gallary
-
-
-
- /* ==========================================================================
-      COUNTER UP 
- ========================================================================== */
-
- $('.counter').counterUp({
-  delay: 10,
-  time: 1000
-});
-
-// latest posts cycle2 carousel
-$( '.slideshow' ).cycle();
-
-
-
-
-    }); // end DOM ready
-  })(jQuery); // end jQuery
-
-  let mainNav = document.getElementById('js-menu');
-  let navBarToggle = document.getElementById('js-navbar-toggle');
-  
-  navBarToggle.addEventListener('click', function () {
-      
-      mainNav.classList.toggle('active');
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
 
 
 
